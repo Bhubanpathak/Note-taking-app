e444545928eac13b09070e453b7d88766f91fbfd
Bhuban Pathak <bhubanpathak05@gmail.com>
2024-02-09 13:03:20 +0530

    Initial commit on new-start branch

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"]
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        src="/Notion.png"
+        height="40"
+        width="40"
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/Notion-Logo-PNG-Pic.png"
+        height="40"
+        width="40"
+        alt="Logo"
+        className="hidden dark:block"
+      />
+     
+    </div>
+  )
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://game-bream-93.clerk.accounts.dev",
+      applicationID: "convex",
+    }
+  ]
+}
